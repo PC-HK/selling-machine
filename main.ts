c0ad5f645c2ecfd944e90f418477173e34f671cd
PC-HK <78074625+PC-HK@users.noreply.github.com>
2021-02-24 16:38:51 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+basic.showArrow(ArrowNames.East)
+servos.P0.setAngle(90)
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showArrow(ArrowNames.South)
+        servos.P0.setAngle(150)
+        basic.pause(1500)
+        servos.P0.setAngle(90)
+        basic.showArrow(ArrowNames.East)
+    }
 })
