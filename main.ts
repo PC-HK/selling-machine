bacc13bb94d9b65b0a36daa95b3704dc87c3775b
PC-HK <78074625+PC-HK@users.noreply.github.com>
2021-02-24 16:41:18 +0800

    1500>300ms

--- main.ts
+++ main.ts
@@ -4,8 +4,9 @@ basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
         basic.showArrow(ArrowNames.South)
         servos.P0.setAngle(150)
-        basic.pause(1500)
+        basic.pause(300)
         servos.P0.setAngle(90)
+        basic.pause(150)
         basic.showArrow(ArrowNames.East)
     }
 })
