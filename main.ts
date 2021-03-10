5f935fec425ae82ea0b7d86ffe63325204cf07a0
PC-HK <78074625+PC-HK@users.noreply.github.com>
2021-03-10 15:33:09 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 basic.showArrow(ArrowNames.East)
 servos.P0.setAngle(90)
 basic.forever(function () {
-    if (input.buttonIsPressed(Button.A)) {
+    if (input.pinIsPressed(TouchPin.P0)) {
         basic.showArrow(ArrowNames.South)
         servos.P0.setAngle(150)
         basic.pause(300)
