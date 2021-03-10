fa814990c40a4d96f6ca264a559a8709d061842b
PC-HK <78074625+PC-HK@users.noreply.github.com>
2021-03-10 15:41:23 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,17 @@
-basic.showArrow(ArrowNames.East)
-servos.P0.setAngle(90)
+function _2 () {
+    basic.showArrow(ArrowNames.West)
+    servos.P0.setAngle(90)
+}
+function _ () {
+    basic.showArrow(ArrowNames.SouthWest)
+    servos.P0.setAngle(150)
+    basic.pause(300)
+    basic.pause(150)
+    _2()
+}
+_2()
 basic.forever(function () {
-    if (input.pinIsPressed(TouchPin.P0)) {
-        basic.showArrow(ArrowNames.South)
-        servos.P0.setAngle(150)
-        basic.pause(300)
-        servos.P0.setAngle(90)
-        basic.pause(150)
-        basic.showArrow(ArrowNames.East)
+    if (pins.analogReadPin(AnalogPin.P2) > 800) {
+        _()
     }
 })
